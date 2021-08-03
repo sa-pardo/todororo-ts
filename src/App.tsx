07f56ce6dc099cfd70b3a54ac8e7445d41e004b8
Sebastian Pardo <sapardosuazo@gmail.com>
2021-08-03 18:31:30 -0400

    adds credits

--- src/App.tsx
+++ src/App.tsx
@@ -42,13 +42,18 @@ function App() {
         <img
           src={rectangle}
           alt="site background"
-          className="absolute left-0 top-0 h-screen w-1/2 z-[-10]"
+          className="absolute left-0 top-0 h-screen w-[53vw] z-[-10]"
         />
       ) : (
         <div className="absolute left-0 top-0 h-screen w-full bg-[#e45858] z-[-10]" />
       )}
       <div className="w-full md:w-1/2 h-screen flex items-center justify-center">
         <Pomodoro selectedTask={selectedTask} />
+        {mediaQuery && (
+          <span className="text-white text-3xl font-extralight right-[52%] bottom-[1%] absolute">
+            Designed by
+          </span>
+        )}
       </div>
       <div className="w-full md:w-1/2 h-screen flex md:items-center justify-center pt-4">
         <TaskList
@@ -58,7 +63,18 @@ function App() {
           selectTask={selectTask}
           addTask={addTask}
         />
+        {mediaQuery && (
+          <span className="text-3xl font-extralight left-[51%] bottom-[1%] absolute">
+            Sebastian Pardo
+          </span>
+        )}
       </div>
+      {!mediaQuery && (
+        <div className="w-full flex justify-center pt-4">
+          <span className="text-[#e45858] mr-2">Designed by</span>
+          Sebastian Pardo
+        </div>
+      )}
     </div>
   );
 }
